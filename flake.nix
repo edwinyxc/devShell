a81cff76aeb4b4dd459c2565dcbfc394aecfb31a
edwin <edwinyxc@outlook.com>
2024-03-29 01:16:52 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "This is the devShell";
+
+  # Flake inputs
+  inputs = {
+    #nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+    nixpkgs.url = "nixpkgs";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs, }: let
+    # Systems supported
+    allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+    ];
+
+    # Helper to provide system-specific attributes
+    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: 
+        f { pkgs = import nixpkgs { inherit system; };
+    });
+  in {
+
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = let
+          # Use Python 3.11
+          python = pkgs.python311;
+
+        in pkgs.mkShell {
+          name = "";
+          # The Nix packages provided in the environment
+          packages = [
+            # Python plus helper tools
+            (python.withPackages (ps:
+              with ps; [
+              
+                # notebook
+                jupyter
+                jupyterlab
+                ipykernel
+                ipython
+
+                # scientific computing
+                pandas # Data structures & tools
+                numpy # Array & matrices
+                scipy # Integral, solving differential, equations, optimizations)
+
+                # Visualization
+                matplotlib # plot & graphs
+                seaborn # heat maps, time series, violin plot
+
+                # Algorithmic Libraries
+                #scikit-learn-extra # Machine learning: regression, classificatons,..
+                scikit-learn # Machine learning: regression, classificatons,..
+                statsmodels # Ecplore data, estimate statistical models, & perform statistical test.
+
+                # Formatting
+                black
+
+                numpy
+                beautifulsoup4
+
+                #mne-python
+                # Probabilistic
+                # pymc #support < 311
+
+              ]))
+          ];
+
+        GREETING = "Python DS environment";
+        
+        shellHook = ''
+            echo $GREETING 
+        '';
+
+        };
+      });
+  };
+}
